1b422aa1fb8302ca772face682d93b0ddccf733b
Bettor <be0128st@gmail.com>
2024-04-16 20:27:07 +0900

    [refactor, 최종] Frag2->Activity
    binding, onDestroyView 쪽은 설명이 없어서 정리가 필요하다.

--- app/src/main/java/com/example/fragmenttest/SecondFragment.kt
+++ app/src/main/java/com/example/fragmenttest/SecondFragment.kt
@@ -1,5 +1,6 @@
 package com.example.fragmenttest
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -19,7 +20,26 @@ private const val ARG_PARAM1 = "param1"
 class SecondFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
-    private val binding by lazy { FragmentSecondBinding.inflate(layoutInflater)}
+
+    // [3] SecondFragment -> Activity
+    // 3-4. Settings
+    private var listener: FragmentDataListener? = null // interface를 가용한 변수 선언
+    private var _binding: FragmentSecondBinding? = null //
+    private val binding get() = _binding!!
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        // [3] SecondFragment -> Activity
+        // 3-5. 메인 액티비와 연결하기.
+        if (context is FragmentDataListener) {
+            listener = context // 연결해버리기
+        } else {
+            throw RuntimeException("$context must implement FragmentDataListener")
+        // 메인액티비티와 연결(onAttach) 되어있어야 데이터를 보낼 수 있다.
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,14 +48,13 @@ class SecondFragment : Fragment() {
         }
     }
 
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_second, container, false)
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -67,7 +86,20 @@ class SecondFragment : Fragment() {
         // 2-2. 두 방법 모두 아래 코드로 구현 가능하다. [끝]
         binding.tvFrag2Text.text = param1
 
+        // [3] SecondFragment -> Activity
+        // 3-6. 데이터 보내, MainActivity에 가보자.
+        binding.btnSendActivity.setOnClickListener{
+            val dataToSend = "Hello from SecondFragment!"
+            listener?.onDataReceived(dataToSend)
+        }
 
 
+
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // Binding 객체 해제
+        _binding = null
+        listener = null
     }
 }
